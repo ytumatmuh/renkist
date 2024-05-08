8031546a6781199d2b0f6ef1ffd8a2666f01cf78
yusssss <yusuf.acmaci@hotmail.com>
2024-05-08 12:17:32 +0300

    bugfix on code

--- src/utils/videoo.py
+++ src/utils/videoo.py
@@ -1,12 +1,14 @@
 import cv2
 
-vidcap = cv2.VideCapture('/../../video_data/vid.mp4')
+vidcap = cv2.VideoCapture('video_data/vid.mp4')
 success, image = vidcap.read() 
 count = 1 
 while success:
-    cv.imwrite("/../../image_data/image_%d.jpg" % count, image)
+    cv2.imwrite("image_data/image_%d.jpg" % count, image)
     print('Saved Image %', count)
     count += 1
     success, image = vidcap.read()
+    
+print('Toplam fotoğraf sayısı: ', count-1)
 
 vidcap.release()
