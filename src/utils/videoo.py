48b2dc1701f42878e5f4b42261a407517f2c07c7
Onur-TURAN <onurturan.t@gmail.com>
2024-06-22 21:49:14 +0300

    emptyupdate

--- src/utils/videoo.py
+++ src/utils/videoo.py
@@ -1,11 +1,11 @@
 import cv2
 import os
 
-vidcap = cv2.VideoCapture('video_data/vid.mp4')
+vidcap = cv2.VideoCapture('../../video_data/vid.mp4')
 success, image = vidcap.read() 
 count = 1 
 while success:
-    cv2.imwrite("image_data/image_%d.jpg" % count, image)
+    cv2.imwrite("../../image_data/image_%d.jpg" % count, image)
     print('Saved Image %', count)
     count += 1
     success, image = vidcap.read()
@@ -13,15 +13,15 @@ while success:
 print('Toplam fotoğraf sayısı: ', count-1)
 
 vidcap.release()
+delete_images()
 
 
 
-
-def delete_images():
-    directory = "image_data"
+ def delete_images():
+    directory = "../../image_data"
     for filename in os.listdir(directory):
         if filename.endswith(".jpg"):
             os.remove(os.path.join(directory, filename))
     print("Tüm fotoğraflar silindi.")
-    
+ 
 #delete_images() fotoları silmek için aktif et
