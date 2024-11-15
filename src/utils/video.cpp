f11a48e7fcc4840c495bb4575bd7f320517a569c
Onur-TURAN <onurturan.t@gmail.com>
2024-11-15 15:29:40 +0300

    videparse

--- src/utils/video.cpp
+++ src/utils/video.cpp
@@ -7,8 +7,8 @@ using namespace cv;
 
 
 int main() {
-    char* video_path = "./../video_data/vid.mp4";
-    Videocapture v_capture("./../video_data/vid.mp4");
+    const char* video_path = "./../video_data/vid.mp4";
+    VideoCapture v_capture(video_path);
     if (!v_capture.isOpened()) {
         cout << "Error opening video file" << endl;
         return -1;
